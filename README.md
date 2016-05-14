be6654bbda3132b8ce846a077efb7640345bda32
meetbrij <meetbrij@gmail.com>
2016-05-14 18:50:19 +0800

    adding working files for the heroes app

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# Angular 2 Hello World app 
+# Angular 2 Heroes Tour app 
 
 Tools used
 - node and npm
 
-A basic Angular Hello World app.
+Angular 2 App for Heroes Tour as per the tutorial on their website.
 
 clone repo and run the following commands
 `npm start`
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,26 +1,46 @@
-import {Component} from '@angular/core';
+import { Component }       from '@angular/core';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
-export class Hero {
-	id: number;
-	name: string;
-}
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  }
+])
 
 @Component({
-    selector: 'my-app',
-    template: `
-		<h1>{{title}}</h1>
-		<h2>{{hero.name}} details!</h2>
-		<div><label>id: </label>{{hero.id}}</div>
-		<div>
-			<label>name: </label>
-			<input [(ngModel)]="hero.name" placeholder="name">
-		</div>
-		`
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
+      <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['app/app.component.css']
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    ROUTER_PROVIDERS,
+    HeroService
+  ]
 })
-export class AppComponent { 
-	title = 'Tour of Heroes';
-	hero: Hero = {
-		id: 1,
-		name: 'Windstorm'
-	};
+export class AppComponent {
+  title = 'Tour of Heroes';
 }
